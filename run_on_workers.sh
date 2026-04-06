8d2e992f9866c605738924f7b51b23efd530cf2f
AAUKajetan <wg38up@aau.student.dk>
2026-04-06 16:01:30 +0200

    run on all

--- run_on_workers.sh
+++ run_on_workers.sh
@@ -3,7 +3,10 @@
 # Example: ./run-on-workers.sh "sudo apt install -y openjdk-17-jdk"
 
 NODES=(10.92.0.211 10.92.0.87 10.92.0.160 10.92.0.16)
+SUDO_PASS="dask"
 CMD="$*"
+USER="dask"
+VENV_ACTIVATE="source ~/nsc_dask/dask-env/bin/activate"
 
 if [ -z "$CMD" ]; then
   echo "Usage: $0 \"command\""
@@ -14,11 +17,14 @@ echo "Running: $CMD"
 echo "---"
 
 for host in "${NODES[@]}"; do
-  echo "=== $host ==="
+  echo "=== $USER@$host ==="
   if [[ "$CMD" == sudo* ]]; then
-    ssh "ubuntu@$host" "cd /home/hadoop && sudo bash -c '${CMD#sudo }'"
+    ssh -i ~/.ssh/id_ed25519_bdm "$USER@$host" "cd /home/$USER && $VENV_ACTIVATE && echo $SUDO_PASS | sudo -S bash -c '${CMD#sudo }'"
+  elif [[ "$CMD" == nohup* ]]; then
+    # Background commands - use -f to not wait
+    ssh -f -i ~/.ssh/id_ed25519_bdm "$USER@$host" "cd /home/$USER && $VENV_ACTIVATE && $CMD"
   else
-    ssh "ubuntu@$host" "cd /home/hadoop && $CMD"
+    ssh -i ~/.ssh/id_ed25519_bdm "$USER@$host" "cd /home/$USER && $VENV_ACTIVATE && $CMD"
   fi
 done
 
